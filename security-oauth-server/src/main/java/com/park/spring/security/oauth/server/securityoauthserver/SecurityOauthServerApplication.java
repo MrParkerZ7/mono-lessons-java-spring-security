8a3463aff06d004ce300cb11598609ac9c40baaa
Park <neverzero9@gmail.com>
2018-01-06 03:34:06 +0700

    Add Security Oauth

---
+++ security-oauth-server/src/main/java/com/park/spring/security/oauth/server/securityoauthserver/SecurityOauthServerApplication.java
@@ -0,0 +1,12 @@
+package com.park.spring.security.oauth.server.securityoauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityOauthServerApplication.class, args);
+	}
+}
